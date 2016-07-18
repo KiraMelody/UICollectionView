f914db5f304eb75f3ab2ce59e4dfa9d44e7b8fbd
KiraMelody <jessie_tjq@163.com>
2016-07-18 16:33:08 +0800

    finish CollectionView(ugly)

---
+++ UICollectionView/ViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  UICollectionView
+//
+//  Created by Brian Coleman on 2014-09-04.
+//  Copyright (c) 2014 Brian Coleman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+    
+    @IBOutlet var collectionView: UICollectionView?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 100, left: 50, bottom: 50, right: 50)
+        layout.itemSize = CGSize(width: 90, height: 90)
+        layout.minimumInteritemSpacing = 0.0;
+        collectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: layout)
+        collectionView!.dataSource = self
+        collectionView!.delegate = self
+        collectionView!.registerClass(CollectionViewCell.self, forCellWithReuseIdentifier: "CollectionViewCell")
+        collectionView!.backgroundColor = UIColor.whiteColor()
+        self.view.addSubview(collectionView!)
+    }
+
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return content.count;
+    }
+    
+    var content = ["啦啦啦","哈哈哈","呵呵呵","哦哦哦","嗯嗯嗯","呱呱呱"]
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("CollectionViewCell", forIndexPath: indexPath) as! CollectionViewCell
+        //cell.backgroundColor = UIColor.grayColor()
+        cell.layer.borderColor = UIColor.grayColor().CGColor
+        cell.layer.borderWidth = 1
+        cell.layer.cornerRadius = 8
+        cell.textLabel?.text = "\(content[indexPath.row])"
+        cell.imageView?.image = UIImage(named: "Unknown-\(indexPath.row+1).png")
+        return cell
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
